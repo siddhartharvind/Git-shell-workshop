093417cb2929c1b7023f8f55250aecf9c0b24488
siddhartharvind <sidarvind11@gmail.com>
2023-09-02 22:13:07 +0530

    Solution for Python: Task 4

--- script.sh
+++ script.sh
@@ -11,22 +11,32 @@ do
 		input_file=input$i.txt
 		exp_output_file=output$i.txt
 		user_output_file=user_${exp_output_file}
-		
+		error_file=error_file.txt
 
 		echo "Running test case $i:"
-		python3 $file < $input_file > $user_output_file
-		passed=$(diff $user_output_file $exp_output_file)
+		python3 $file < $input_file > $user_output_file 2> $error_file
+		exit_code=$?
 
-		if [[ "$passed" == "" ]]
+		if [[ $exit_code == 0 ]]
 		then
-			echo "Test case $i passed"
+			echo "Code ran without run-time errors"
+
+			passed=$(diff $user_output_file $exp_output_file)
+			if [[ "$passed" == "" ]]
+			then
+				echo "Test case $i passed"
+			else
+				echo "Test case $i failed"
+				echo "User output:"
+				cat $user_output_file
+				echo "Expected output:"
+				cat $exp_output_file
+			fi
+
 		else
-			echo "Test case $i failed"
-			echo "User output:"
-			cat $user_output_file
-			echo "Expected output:"
-			cat $exp_output_file
-		fi
+			echo "Code has run-time errors"
+			cat $error_file
+		fi		
 
 		echo "--------------------------------"
 		rm ./$user_output_file
@@ -34,10 +44,4 @@ do
 	done
 done
 
-echo "The language passed as argument is $language"
-
-echo "User output:"
-python3 $filename < input1.txt
-
-echo "Expected output:"
-cat output1.txt
+rm ./$error_file

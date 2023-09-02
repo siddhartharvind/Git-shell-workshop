b214932839e1005b0349f133b770f8ce9dad0ba0
siddhartharvind <sidarvind11@gmail.com>
2023-09-02 21:41:56 +0530

    Fixed issue where output files with wrong names produced and persisted

--- script.sh
+++ script.sh
@@ -17,29 +17,36 @@ else
 		for i in 1 2 3
 		do
 			echo "Running test case $i:"
-			./file.out < input$i.txt > user_output$i.txt 2> $file_error$i.txt
+			input_file=input$i.txt				# input file
+			user_output_file=user_output$i.txt	# actual output
+			exp_output_file=output$i.txt		# expected output
+			err_file=${file}_error${i}.txt
+			./file.out < $input_file > $user_output_file 2> $err_file
 			exit_code=$?
 
 			if [[ $exit_code -eq 0 ]]
 			then
 				echo "Code ran without run-time errors"
 
-				passed=$(diff output$i.txt user_output$i.txt)
+				passed=$(diff $exp_output_file $user_output_file)
 				if [[ "$passed" == "" ]]
 				then
 					echo "Test case $i passed"
 				else
 					echo "Test case $i failed"
 					echo "User output:"
-					cat user_output$i.txt
+					cat $user_output_file
 					echo "Expected output:"
-					cat output$i.txt
+					cat $exp_output_file
 				fi
 
 			else
 				echo "Code has run-time errors"
-				cat $file_error$i.txt
+				cat $err_file
 			fi
+
+			rm ./$user_output_file
+			rm ./$err_file
 
 			echo "------------------------------------------"
 		done

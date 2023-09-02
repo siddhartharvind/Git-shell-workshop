df1a231055f7beea84d4f419a2f369c147a40fc7
siddhartharvind <sidarvind11@gmail.com>
2023-09-02 18:13:32 +0530

    Solution for C: Task 3

--- script.sh
+++ script.sh
@@ -21,20 +21,50 @@
 
 
 
-# Task 2
+### Task 2
+# for file in $*
+# do
+# 	echo "Running $file on test cases"
+# 	for i in 1 2 3
+# 	do
+# 		echo "Running test case $i:"
+# 		gcc file$i.c
+# 		echo "User output:"
+# 		./a.out < input$i.txt
+# 		echo "Expected output:"
+# 		cat output$i.txt
+# 		echo "------------------------------------------"
+# 	done
+# done
+
+# rm ./a.out
+
+
+
+
+### Task 3
 for file in $*
 do
+	gcc $file -o file.out
 	echo "Running $file on test cases"
 	for i in 1 2 3
 	do
 		echo "Running test case $i:"
-		gcc file$i.c
-		echo "User output:"
-		./a.out < input$i.txt
-		echo "Expected output:"
-		cat output$i.txt
+		# echo "./file.out < input$i.txt > user_output$i.txt 2> /dev/null"
+		./file.out < input$i.txt > user_output$i.txt 2> /dev/null
+
+		passed=$(diff output$i.txt user_output$i.txt)
+		if [[ "$passed" == "" ]]
+		then
+			echo "Test case $i passed"
+		else
+			echo "Test case $i failed"
+			echo "User output:"
+			cat user_output$i.txt
+			echo "Expected output:"
+			cat output$i.txt
+		fi
 		echo "------------------------------------------"
 	done
+	rm ./file.out
 done
-
-rm ./a.out

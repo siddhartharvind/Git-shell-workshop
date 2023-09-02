1dac5f2e3c643057aae36fcaf6d31300b812188f
siddhartharvind <sidarvind11@gmail.com>
2023-09-02 21:59:50 +0530

    Solution for Python: Task 2

--- script.sh
+++ script.sh
@@ -1,13 +1,22 @@
-# Task 1: 
-# The first argument provided from cmd must be the language you are running it in
-# Just plainly print the first argument that was provided 
-# The second argument is the name of the python/C file that must be run 
-# Run the python file, using input1.txt as stdin, and write the output to a file user_output1.txt 
-# Display user_output1.txt to the user as user output, and the file output1.txt as the expected output 
-# Once you have tested out the script and verified it's working, commit it to a branch: <your_name>
+#!/usr/bin/env bash
 
-language=$1
-filename=$2
+files=$*
+
+for file in $files
+do
+	echo "Running $file on test cases"
+
+	for i in $(seq 3)
+	do
+		echo "Running test case $i:"
+		echo "User output:"
+		python3 $file < input$i.txt
+		echo "Expected output:"
+		cat output$i.txt
+		echo "--------------------------------"
+	done
+
+done
 
 echo "The language passed as argument is $language"
 

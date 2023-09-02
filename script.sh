acd2c13c7252f5806fb4f071fc5d73eda860bb21
siddhartharvind <sidarvind11@gmail.com>
2023-09-02 22:21:16 +0530

    Flattened code to remove unnecessary else on main body

--- script.sh
+++ script.sh
@@ -1,56 +1,58 @@
-### Done Task 5
+#!/usr/bin/env bash
 
 language=$1
 files=${*:2}
+
 echo "The language passed as argument is $language"
 
 if [[ "$language" != "C" ]]
 then
 	echo "Invalid language"
-
+	exit 1
 else
+	echo "Language verified as C"
+fi
 
-	for file in $files
+for file in $files
+do
+	gcc $file -o file.out
+	echo "Running $file on test cases"
+	for i in 1 2 3
 	do
-		gcc $file -o file.out
-		echo "Running $file on test cases"
-		for i in 1 2 3
-		do
-			echo "Running test case $i:"
-			input_file=input$i.txt				# input file
-			user_output_file=user_output$i.txt	# actual output
-			exp_output_file=output$i.txt		# expected output
-			err_file=${file}_error${i}.txt
-			./file.out < $input_file > $user_output_file 2> $err_file
-			exit_code=$?
+		echo "Running test case $i:"
+		input_file=input$i.txt				# input file
+		user_output_file=user_output$i.txt	# actual output
+		exp_output_file=output$i.txt		# expected output
+		err_file=${file}_error${i}.txt
+		./file.out < $input_file > $user_output_file 2> $err_file
+		exit_code=$?
 
-			if [[ $exit_code -eq 0 ]]
+		if [[ $exit_code -eq 0 ]]
+		then
+			echo "Code ran without run-time errors"
+
+			passed=$(diff $exp_output_file $user_output_file)
+			if [[ "$passed" == "" ]]
 			then
-				echo "Code ran without run-time errors"
-
-				passed=$(diff $exp_output_file $user_output_file)
-				if [[ "$passed" == "" ]]
-				then
-					echo "Test case $i passed"
-				else
-					echo "Test case $i failed"
-					echo "User output:"
-					cat $user_output_file
-					echo "Expected output:"
-					cat $exp_output_file
-				fi
-
+				echo "Test case $i passed"
 			else
-				echo "Code has run-time errors"
-				cat $err_file
+				echo "Test case $i failed"
+				echo "User output:"
+				cat $user_output_file
+				echo "Expected output:"
+				cat $exp_output_file
 			fi
 
-			rm ./$user_output_file
-			rm ./$err_file
+		else
+			echo "Code has run-time errors"
+			cat $err_file
+		fi
 
-			echo "------------------------------------------"
-		done
-		rm ./file.out
+		rm ./$user_output_file
+		rm ./$err_file
+
+		echo "------------------------------------------"
 	done
+	rm ./file.out
+done
 
-fi
